222a85a3c54741a248d7566c4602bfd8525b94e5
Tin Tran <tintranfs@gmail.com>
2025-01-12 08:49:26 +0700

    Update code

--- packages/shared-web/__tests__/unit/libs/msw/browser.ts
+++
@@ -1,7 +0,0 @@
-import { http } from 'msw';
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers';
-
-const worker = setupWorker(...handlers);
-
-export { http, worker };
