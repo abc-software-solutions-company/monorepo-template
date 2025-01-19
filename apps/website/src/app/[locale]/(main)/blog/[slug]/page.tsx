eecd81c5801b0270ab385f02210b5fc0924476cc
Thien Lam <lamminhthien02012000@gmail.com>
2025-01-19 08:03:51 +0700

    test AWS ECR with admin portal, api and website

--- apps/website/src/app/[locale]/(main)/blog/[slug]/page.tsx
+++ apps/website/src/app/[locale]/(main)/blog/[slug]/page.tsx
@@ -29,54 +29,54 @@ export default async function PostDetailPage(pageProps: PageProps) {
   );
 }
 
-export async function generateStaticParams() {
-  const postsResponse = await PostApi.getServerPosts({ page: 1, limit: 30 });
+// export async function generateStaticParams() {
+//   const postsResponse = await PostApi.getServerPosts({ page: 1, limit: 30 });
 
-  return postsResponse.data.map(post => ({ slug: post.slug }));
-}
+//   return postsResponse.data.map(post => ({ slug: post.slug }));
+// }
 
-export async function generateMetadata({ params: { locale, slug } }: PageProps): Promise<Metadata> {
-  const response = await PostApi.getServerPost(slug);
+// export async function generateMetadata({ params: { locale, slug } }: PageProps): Promise<Metadata> {
+//   const response = await PostApi.getServerPost(slug);
 
-  const defaultLanguage = LANGUAGES.find(x => x.isDefault)?.code;
-  const title = response.data.nameLocalized?.find(x => x.lang === locale)?.value ?? '';
-  const description = response.data.descriptionLocalized?.find(x => x.lang === locale)?.value ?? '';
-  const image = response.data.coverLocalized?.find(x => x.lang === locale)?.value ?? '';
-  const creator = response.data.creator.name;
-  const createdAt = response.data.createdAt.toString();
+//   const defaultLanguage = LANGUAGES.find(x => x.isDefault)?.code;
+//   const title = response.data.nameLocalized?.find(x => x.lang === locale)?.value ?? '';
+//   const description = response.data.descriptionLocalized?.find(x => x.lang === locale)?.value ?? '';
+//   const image = response.data.coverLocalized?.find(x => x.lang === locale)?.value ?? '';
+//   const creator = response.data.creator.name;
+//   const createdAt = response.data.createdAt.toString();
 
-  const seoTitle = response.data.seoMeta?.titleLocalized?.find(x => x.lang === locale)?.value ?? title;
-  const seoDescription = response.data.seoMeta?.descriptionLocalized?.find(x => x.lang === locale)?.value ?? stripHTML(description);
-  const keywords = response.data.seoMeta?.keywords ?? '';
+//   const seoTitle = response.data.seoMeta?.titleLocalized?.find(x => x.lang === locale)?.value ?? title;
+//   const seoDescription = response.data.seoMeta?.descriptionLocalized?.find(x => x.lang === locale)?.value ?? stripHTML(description);
+//   const keywords = response.data.seoMeta?.keywords ?? '';
 
-  const ogImage = image ? `${process.env.NEXT_PUBLIC_API_URL}/${image}` : WEBSITE_OG_IMAGE;
-  const languages = LANGUAGES.reduce(
-    (acc, lang) => {
-      acc[lang.code] = lang.code === defaultLanguage ? `/blog/${slug}` : `/${lang.code}/blog/${slug}`;
+//   const ogImage = image ? `${process.env.NEXT_PUBLIC_API_URL}/${image}` : WEBSITE_OG_IMAGE;
+//   const languages = LANGUAGES.reduce(
+//     (acc, lang) => {
+//       acc[lang.code] = lang.code === defaultLanguage ? `/blog/${slug}` : `/${lang.code}/blog/${slug}`;
 
-      return acc;
-    },
-    {} as Record<string, string>
-  );
+//       return acc;
+//     },
+//     {} as Record<string, string>
+//   );
 
-  return {
-    title: seoTitle,
-    description: seoDescription,
-    keywords: keywords.split(', '),
-    creator,
-    authors: { name: creator },
-    openGraph: {
-      title: seoTitle,
-      description: seoDescription,
-      images: [{ url: ogImage, alt: seoTitle }],
-      type: 'article',
-      publishedTime: createdAt,
-      authors: [creator],
-    },
-    twitter: { title: seoTitle, description: seoDescription, images: { url: ogImage, alt: seoTitle } },
-    alternates: {
-      canonical: locale === defaultLanguage ? `/blog/${slug}` : `/${locale}/blog/${slug}`,
-      languages,
-    },
-  };
-}
+//   return {
+//     title: seoTitle,
+//     description: seoDescription,
+//     keywords: keywords.split(', '),
+//     creator,
+//     authors: { name: creator },
+//     openGraph: {
+//       title: seoTitle,
+//       description: seoDescription,
+//       images: [{ url: ogImage, alt: seoTitle }],
+//       type: 'article',
+//       publishedTime: createdAt,
+//       authors: [creator],
+//     },
+//     twitter: { title: seoTitle, description: seoDescription, images: { url: ogImage, alt: seoTitle } },
+//     alternates: {
+//       canonical: locale === defaultLanguage ? `/blog/${slug}` : `/${locale}/blog/${slug}`,
+//       languages,
+//     },
+//   };
+// }
