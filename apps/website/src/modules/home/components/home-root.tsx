3d89cd891d8feec588e7ba86f94119a6b1724820
Nhi Lê <120070400+nhile115@users.noreply.github.com>
2025-01-24 15:02:15 +0700

    Revert "[PT] Create post"
    This reverts commit 6805963d012b11cc2656748d18393224558390b0.

--- apps/website/src/modules/home/components/home-root.tsx
+++ apps/website/src/modules/home/components/home-root.tsx
@@ -1,14 +1,5 @@
 import React, { FC } from 'react';
 import classNames from 'classnames';
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from '@repo/react-web-ui-shadcn/components/ui/select';
 
 import { ComponentBaseProps } from '@/interfaces/component.interface';
 
@@ -31,21 +22,6 @@ const HomeRoot: FC<ComponentBaseProps> = ({ className }) => {
       <NewsLetterRoot />
       <FrequentlyAskedQuestions />
       <ContactRoot />
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select a fruit" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Fruits</SelectLabel>
-            <SelectItem value="apple">Apple</SelectItem>
-            <SelectItem value="banana">Banana</SelectItem>
-            <SelectItem value="blueberry">Blueberry</SelectItem>
-            <SelectItem value="grapes">Grapes</SelectItem>
-            <SelectItem value="pineapple">Pineapple</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
     </div>
   );
 };
