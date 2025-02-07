e012b00c8723c7bbbf489b8a28f0c68af44739ac
Tin Tran <tintranfs@gmail.com>
2025-02-07 16:15:52 +0700

    Add categories api (#106)

---
+++ apps/api/src/modules/categories/categories.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, HttpStatus, Param, Query } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+
+import { ApiDocumentResponse } from '@/common/decorators/api-document-response.decorator';
+import { PaginatedResponse } from '@/common/decorators/paginated-response.decorator';
+import { Response } from '@/common/decorators/response.decorator';
+
+import { GetCategoriesSuccessDoc, GetCategoryFailureDoc, GetCategorySuccessDoc } from './docs/categories.doc';
+import { FilterCategoryDto } from './dto/filter-category.dto';
+import { CategoriesService } from './categories.service';
+
+@Controller('categories')
+@ApiTags('Categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get categories' })
+  @ApiDocumentResponse({ status: HttpStatus.OK, message: 'Get categories successfully', model: GetCategoriesSuccessDoc })
+  @PaginatedResponse({ message: 'Get categories successfully' })
+  find(@Query() filterDto: FilterCategoryDto) {
+    return this.categoriesService.find(filterDto);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get category' })
+  @ApiDocumentResponse({ status: HttpStatus.OK, message: 'Get category successfully', model: GetCategorySuccessDoc })
+  @ApiDocumentResponse({ status: HttpStatus.NOT_FOUND, message: 'Category not found', model: GetCategoryFailureDoc })
+  @Response({ status: HttpStatus.OK, message: 'Get category successfully' })
+  @ApiParam({ name: 'id', description: 'UUID', example: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' })
+  findOne(@Param('id') id: string) {
+    return this.categoriesService.findOne(id);
+  }
+}
--- apps/api/src/modules/categories/categories.module.ts
+++ apps/api/src/modules/categories/categories.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { CategoryFile } from './entities/category-file.entity';
 import { AdminCategoriesController } from './admin-categories.controller';
+import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
 
 import { AuditLogsService } from '../audit-logs/audit-logs.service';
@@ -12,7 +13,7 @@ import { AuditLog } from '../audit-logs/entities/audit-log.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category, CategoryFile, AuditLog])],
-  controllers: [AdminCategoriesController],
+  controllers: [AdminCategoriesController, CategoriesController],
   providers: [CategoriesService, JwtService, AuditLogsService],
   exports: [CategoriesService],
 })
