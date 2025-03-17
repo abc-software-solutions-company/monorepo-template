c41c11cddfa6d53561e88f936c33cb157f83217e
Thiện Lâm <lamminhthien02012000@gmail.com>
2025-03-17 13:38:34 +0700

    [DevOps] Add "Disallow: /" in robots.txt for staging website

--- apps/website/src/app/robots.ts
+++ apps/website/src/app/robots.ts
@@ -2,16 +2,18 @@ import { MetadataRoute } from 'next';
 
 import { WEBSITE_URL } from '@/constants/site.constant';
 
+const isProduction = process.env.NEXT_PUBLIC_APP_ENV === 'production';
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
-        disallow: ['/private/', '/protected/', '/admin/'],
+        allow: isProduction ? '/' : '',
+        disallow: isProduction ? ['/private/', '/protected/', '/admin/'] : ['/'],
       },
     ],
     host: WEBSITE_URL,
-    sitemap: [`${WEBSITE_URL}/sitemap.xml`, `${WEBSITE_URL}/sitemap-0.xml`, `${WEBSITE_URL}/sitemap-1.xml`],
+    sitemap: isProduction ? [`${WEBSITE_URL}/sitemap.xml`, `${WEBSITE_URL}/sitemap-0.xml`, `${WEBSITE_URL}/sitemap-1.xml`] : [],
   };
 }
