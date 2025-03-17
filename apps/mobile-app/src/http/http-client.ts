31998d2e223129712f437b29c8c77e13ef65b6ea
Thiện Lâm <lamminhthien02012000@gmail.com>
2025-03-17 12:07:32 +0700

    [DevOps] Fix issue CORS for API Post contacts on website (For stage environment)

--- apps/mobile-app/src/http/http-client.ts
+++ apps/mobile-app/src/http/http-client.ts
@@ -2,12 +2,13 @@ import Axios from 'axios';
 import Config from 'react-native-config';
 
 const defaultBaseURL = Config.RN_PUBLIC_API ?? 'http://localhost:3500';
+const isProduction = Config.APP_ENV === 'production';
 
 export const createAxiosInstance = (baseURL = defaultBaseURL) => {
   return Axios.create({
     baseURL,
     timeout: 90000,
-    withCredentials: true,
+    withCredentials: isProduction,
     headers: {
       'Content-Type': 'application/json',
     },
--- apps/website/src/http/http-client.ts
+++ apps/website/src/http/http-client.ts
@@ -1,12 +1,13 @@
 import Axios from 'axios';
 
 const defaultBaseURL = process.env.NEXT_PUBLIC_API_URL;
+const isProduction = process.env.NEXT_PUBLIC_APP_ENV === 'production';
 
 export const createAxiosInstance = (baseURL = defaultBaseURL) => {
   return Axios.create({
     baseURL,
     timeout: 90000,
-    withCredentials: true,
+    withCredentials: isProduction,
     headers: {
       'Content-Type': 'application/json',
     },
