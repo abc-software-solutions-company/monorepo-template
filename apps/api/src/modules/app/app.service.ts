35fa012e32639174d73361ef1c6b529732f363df
Thiện Lâm <lamminhthien02012000@gmail.com>
2025-06-14 06:19:37 +0700

    Implement kafka on health check

--- apps/api/src/modules/app/app.service.ts
+++ apps/api/src/modules/app/app.service.ts
@@ -1,17 +1,29 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
 
+import { KafkaService } from '@/common/kafka/kafka.service';
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  constructor(@Inject('HERO_SERVICE') private readonly client: ClientKafka) {}
+  constructor(
+    @Inject('HERO_SERVICE') private readonly client: ClientKafka,
+    private readonly kafkaService: KafkaService
+) {}
 
   async onModuleInit() {
     this.client.subscribeToResponseOf('hero.kill.dragon');
 
     await this.client.connect();
+  }
+
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async healthCheckKafka(): Promise<string> {
+    await this.kafkaService.produceMessage('example.topic', { message: 'Hello Kafka!' });
+
+    return 'Kafka health good'
   }
 }
